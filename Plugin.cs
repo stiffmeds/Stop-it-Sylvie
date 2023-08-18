bfcb8c872aff65e4a461c34e725d2d8346ae1a6a
stiffmeds <6158160+stiffmeds@users.noreply.github.com>
2023-08-19 03:54:45 +0800

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Collections.Generic;
+
+namespace Stop_it_Sylvie
+{
+    [BepInPlugin("com.meds.stopitsylvie", "Stop it, Sylvie!", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new("com.meds.stopitsylvie");
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Sylvie better stop it with those sounds.");
+            harmony.PatchAll();
+        }
+    }
+    [HarmonyPatch]
+    public class StopHerPls
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Globals), "CreateGameContent")]
+        public static void CreateGameContentPostfix()
+        {
+            Dictionary<string, SubClassData> medsSubClassesSource = Traverse.Create(Globals.Instance).Field("_SubClassSource").GetValue<Dictionary<string, SubClassData>>();
+            Dictionary<string, SubClassData> medsSubClasses = Traverse.Create(Globals.Instance).Field("_SubClass").GetValue<Dictionary<string, SubClassData>>();
+            medsSubClassesSource["archer"].HitSound = medsSubClassesSource["priest"].HitSound;
+            medsSubClasses["archer"].HitSound = medsSubClasses["priest"].HitSound;
+            Traverse.Create(Globals.Instance).Field("_SubClassSource").SetValue(medsSubClassesSource);
+            Traverse.Create(Globals.Instance).Field("_SubClass").SetValue(medsSubClasses);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# Stop it, Sylvie!
+
+Have you ever wished that Sylvie would stop moaning? Are you Willdroyd? This is the mod for you!
+
+## Installation
+
+Install [BepInEx](https://across-the-obelisk.thunderstore.io/package/BepInEx/BepInExPack_AcrossTheObelisk/).
+
+Download the latest release and put it in your BepInEx _plugins_ folder.
+
+## Support
+
+Ping @stiffmeds in the #modding channel of the [official Across the Obelisk Discord](https://discord.gg/across-the-obelisk-679706811108163701).
